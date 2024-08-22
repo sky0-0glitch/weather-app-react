07f1d4dd68967762c5d471d0a7de424f47db722d
sky0-0glitch <157227184+sky0-0glitch@users.noreply.github.com>
2024-08-22 20:40:20 +0500

    added link to github

--- src/App.js
+++ src/App.js
@@ -3,7 +3,16 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <footer>this page is coded by sahar omer open sourced on gethub</footer>
+      <footer>
+        this page is coded by sahar omer open sourced on{" "}
+        <a
+          href="https://github.com/sky0-0glitch/weather-app-react"
+          target="_blank"
+        >
+          {" "}
+          gethub
+        </a>
+      </footer>
     </div>
   );
 }
