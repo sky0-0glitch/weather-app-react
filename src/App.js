5db3981da28edb980c160704ebbb90f4d052944e
sky0-0glitch <157227184+sky0-0glitch@users.noreply.github.com>
2024-09-04 11:40:45 +0500

    still trying to fix the BUG

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@ import "./App.css";
 import Weather from "./Weather";
 import Video1 from "./Background.mp4";
 import Video2 from "./background.ogv";
-import Video3 from "./background.webm";
+import Video3 from "./background.h264";
+import Video4 from "./background.webm";
+import poster from "./poster.png";
 function App() {
   return (
     <div className="App">
-      <video autoPlay loop muted playsInline>
+      <video autoPlay loop muted playsInline poster={poster}>
         <source src={Video1} />
         <source src={Video2} />
         <source src={Video3} />
+        <source src={Video4} />
       </video>
       <div className="Application">
         <div className="container ">
